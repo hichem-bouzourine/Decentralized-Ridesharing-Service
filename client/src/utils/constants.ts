b749d64920b7b3031c6e754eadf3843e52fc61c2
Hichem <hichembouzourine.uni@gmail.com>
2023-03-19 14:39:47 +0100

    Update contract addresses.

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -6,6 +6,8 @@ export const ClientAbi = clientAbi.abi;
 export const DriverAbi = driverAbi.abi;
 export const RideAbi = rideAbi.abi;
 
-export const ClientContractAddress = `0x51cc75D32F4247183836F54A5266640f0e113296`;
-export const DriverContractAddress = `0x2cca781F349F98121c8966e634731ADb48CCE899`;
-export const RideContractAddress = `0xDB85319302542cE9334D0cd9c3765De0F9333D55`;
+export const ClientContractAddress =
+  "0x7b42c381CD4c278541250e1898f5125006719027";
+export const DriverContractAddress =
+  "0xf810E1160cfF389cE8b16d74968B2BFbaab6acdf";
+export const RideContractAddress = "0x9A5070023aa723d60dfBB3393ef7a15842A9D8F0";
