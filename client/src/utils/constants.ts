01527bc454562b5e1268c3da424a30611bff061b
Hichem <hichembouzourine.uni@gmail.com>
2023-03-19 09:31:00 +0100

    Add contracts Abis.

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,11 @@
+import clientAbi from "./Client.json";
+import driverAbi from "./Driver.json";
+import rideAbi from "./Ride.json";
+
+export const ClientAbi = clientAbi.abi;
+export const DriverAbi = driverAbi.abi;
+export const RideAbi = rideAbi.abi;
+
+export const ClientContractAddress = `0x51cc75D32F4247183836F54A5266640f0e113296`;
+export const DriverContractAddress = `0x2cca781F349F98121c8966e634731ADb48CCE899`;
+export const RideContractAddress = `0xDB85319302542cE9334D0cd9c3765De0F9333D55`;
